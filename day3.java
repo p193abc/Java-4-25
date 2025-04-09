7dd53e41ba28f323ae0e90604e2c90b8ea6d16f6
p193abc <prameshrai2006@gmail.com>
2025-04-09 12:45:59 +0530

    msgh

---
+++ day3.java
@@ -0,0 +1,24 @@
+public class day3 {
+public static void main(String [] args){
+    int a=5;
+    switch (a){
+        case 1:
+        System.out.println("hello");
+        break;
+        case 2:
+        System.out.println("hii");
+        break;
+        case 3:
+        System.out.println("yo");
+        break;
+        case 4:
+        System.out.println("aloha");
+        break;
+        case 5:
+        System.out.println("namaste");
+        break;
+        default:
+        System.out.println("ye hii hello chodo");
+    }
+}
+}
