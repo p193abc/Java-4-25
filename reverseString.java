356c8919d50408e789fca4c256b9309f4f7b950f
p193abc <57997980+p193abc@users.noreply.github.com>
2025-04-10 13:13:17 +0530

    Create reverseString.java

---
+++ reverseString.java
@@ -0,0 +1,22 @@
+
+public class reverseString {
+    
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("hello");
+        System.out.println(sb);
+        
+        for(int i=0;i<sb.length()/2;i++){
+            int front=i;
+            int back =sb.length()-1-i;
+            
+            char frontChar=sb.charAt(front);
+            char backChar=sb.charAt(back);
+            
+            sb.setCharAt(front,backChar);
+            sb.setCharAt(back,frontChar);
+        }
+        System.out.print(sb);      
+        
+    }
+    
+}
