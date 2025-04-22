d631a3540fadc6a8a4a509c4fc756da360e8d060
p193abc <57997980+p193abc@users.noreply.github.com>
2025-04-22 14:56:41 +0530

    msg

---
+++ findEvenOddDigit.java
@@ -0,0 +1,19 @@
+
+public class day7 {
+    
+    public static void main(String[] args) {
+        int n=1234;
+        System.out.println("n="+n);
+     //int   a;
+        while(n>0){
+            int a=n%10;
+            if(a%2==0)
+                System.out.print(a+"=even\n");
+            else
+                System.out.print(a+"=odd\n");
+             n=n/10;     
+        }  
+        
+    }
+    
+}
